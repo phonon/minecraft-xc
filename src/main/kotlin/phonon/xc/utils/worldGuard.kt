2e18b6aacfd143f4a5da16c82a8c3d396673802e
phonon <phonon9000@gmail.com>
2022-07-10 20:36:18 -0400

    fix worldguard checks to be allow by default and only deny with explicit deny flag

--- src/main/kotlin/phonon/xc/utils/worldGuard.kt
+++ src/main/kotlin/phonon/xc/utils/worldGuard.kt
@@ -8,7 +8,7 @@ import org.bukkit.Location
 import com.sk89q.worldguard.WorldGuard
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldguard.protection.flags.Flags
-
+import com.sk89q.worldguard.protection.flags.StateFlag
 
 public object WorldGuard {
     /**
@@ -18,7 +18,8 @@ public object WorldGuard {
         val regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer()
         val query = regionContainer.createQuery()
         val regions = query.getApplicableRegions(BukkitAdapter.adapt(loc))
-        if ( !regions.testState(null, Flags.PVP) ) {
+        // this only blocks if region explicity denies (so pvp on by default)
+        if ( regions.queryState(null, Flags.PVP) == StateFlag.State.DENY ) {
             return false
         }
         return true
@@ -31,7 +32,8 @@ public object WorldGuard {
         val regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer()
         val query = regionContainer.createQuery()
         val regions = query.getApplicableRegions(BukkitAdapter.adapt(loc))
-        if ( !regions.testState(null, Flags.OTHER_EXPLOSION) ) {
+        // this only blocks if region explicity denies (so explosions on by default)
+        if ( regions.queryState(null, Flags.OTHER_EXPLOSION) == StateFlag.State.DENY ) {
             return false
         }
         return true
